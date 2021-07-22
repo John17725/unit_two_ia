3057bdc65fd6aebcd47d436b4a9ec9cb77c28d43
John17725 <barthjoss@hotmail.es>
2021-07-22 16:57:56 -0500

    files to repo third change

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,18 @@
 const input = document.querySelector('input[type="file"]')
 
 
+
+var fr = new FileReader();
+fr.onload=function(){
+  document.getElementById('form76')
+  .textContent=fr.result;
+}
+fr.readAsText(this, files[0]);
+
+input.addEventListener('change', function (e) {
+  handleFiles(input.files)
+}, false)
+
 const btn_enviar_texto = document.getElementById('btn-enviar-texto');
 const btn_escuchar = document.getElementById('btn-escuchar');
 btn_escuchar.addEventListener('click', () => {
