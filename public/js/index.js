98bd0ef0dbaf274fb3a8cc96ece84d32f7de0985
John17725 <barthjoss@hotmail.es>
2021-07-22 22:51:38 -0500

    Primer avance

--- public/js/index.js
+++ public/js/index.js
@@ -1,17 +1,32 @@
-const input = document.querySelector('input[type="file"]')
+var file = document.getElementById('btn-subir-text');
 
+file.addEventListener('change', () => {
+    var txtArr = [];
+    var fr = new FileReader();
+    var cant_numeros = 0;
+    fr.onload = function() {
+        // By lines
+        var lines = this.result.split('\n');
+        for (var line = 0; line < lines.length; line++) {
+            txtArr = [...txtArr, ...(lines[line].split(" "))];
+        }
+    }
+    fr.onloadend = function() {
+        console.log(txtArr);
+        document.getElementById('text-area-salida').textContent=txtArr.join("");
+        var num_palabras = document.getElementById('data-release1');
+        num_palabras.innerHTML = "Numero de palabras: "+txtArr.length;
+        document.getElementById('data-release1').style.display = 'block';
 
+        var num_palabras = document.getElementById('data-release2');
+        num_palabras.innerHTML = "Cantidad de numeros: "+cant_numeros;
+        document.getElementById('data-release2').style.display = 'block';
 
-var fr = new FileReader();
-fr.onload=function(){
-  document.getElementById('form76')
-  .textContent=fr.result;
-}
-fr.readAsText(this, files[0]);
+    }
 
-input.addEventListener('change', function (e) {
-  handleFiles(input.files)
-}, false)
+    fr.readAsText(file.files[0]);
+});
+
 
 const btn_enviar_texto = document.getElementById('btn-enviar-texto');
 const btn_escuchar = document.getElementById('btn-escuchar');
